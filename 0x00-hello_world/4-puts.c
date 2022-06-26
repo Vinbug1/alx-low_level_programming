f4f71fc7af436e08dd7ab088cadf5c56f04c4eec
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2022-06-26 12:55:12 -0700

    Put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  *  * main - Entry point
  *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *     */
+ * Return: 0
+ *
+ */
 
 int main(void)
 {
