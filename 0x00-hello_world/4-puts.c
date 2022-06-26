a832c51537fce9eec55d86cf7533252ca07e620e
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-06-26 21:27:39 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /*
  * main - Entry point
  * description: put "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
