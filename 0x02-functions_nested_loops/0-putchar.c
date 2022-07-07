344d8de1225042c437d9330aac341d1f3cefd940
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-07-07 06:57:50 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,16 @@
 * Return: Always 0 (success)
 */
 int main(void)
- {
-     char str[] = "_putchar\n"; 
-     int i = 0; 
-     while(str[i] != '\0')
-     {
-         char c = str[i];
-         _putchar(c);
-         i++;
-    } 
-    return (0);
- }
+{
+char str[] = "_putchar\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
+}
