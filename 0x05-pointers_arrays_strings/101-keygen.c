29c2bded58b1bb7753b7c3d2264d121440e1c6dd
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-07-12 15:32:57 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,70 +1,26 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
-* Function to randomly generates password
-* of length N
-* Initialize counte
-* Seed the random-number generator
-* with current time so that the
-* numbers will be different every time
-* Array of numbers
-* Array of small alphabets
-* Array of capital alphabets
-* Array of all the special symbols
-* Stores the random password
-* To select the randomizer
-* inside the loop
-* terate over the range [0, N]
-* Driver Code
-* Length of the password to
-* be generated
-* Function Call
-*/
-void randomPasswordGeneration(int N)
-{
-    int i = 0;
-    int randomizer = 0;
-  
-    srand((unsigned int)(time(NULL)));
-  
-    char numbers[] = "0123456789";
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-    char symbols[] = "!@#$^&*?";
-    char password[N];
-  
-    randomizer = rand() % 4;
-  
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
+ * main - generates keygen.
+ * Return: 0
+ */
 
-int main()
+int main(void)
 {
-  int N = 10; 
-  randomPasswordGeneration(N);
-   return 0;
+int r = 0, c = 0;
+time_t t;
+
+srand((unsigned int) time(&t));
+while (c < 2772)
+{
+r = rand() % 128;
+if ((c + r) > 2772)
+break;
+			
+c = c + r;
+printf("%c", r);
+}
+printf("%c\n", (2772 - c));
+return (0);
 }
