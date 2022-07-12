2803f01b5a910a6e0598401b7bf339c25487ea12
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-07-12 13:56:09 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,17 +4,13 @@
 * @s: pointer to the string to print
 * Return: void
 */
-
 void rev_string(char *s)
 {
 int len, i, half;
 char temp;
-	
 for (len = 0; s[len] != '\0'; len++);
-
 i = 0;
 half = len / 2;
-
 while (half--)
 {
 temp = s[len - i - 1];
