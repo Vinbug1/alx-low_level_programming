f5f7f7340044a4415bd742be4c47d5f8dbf5b281
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-07-12 12:57:59 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <main.h>
+/**
+ * rev_string - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
 
 void rev_string(char *s)
 {
