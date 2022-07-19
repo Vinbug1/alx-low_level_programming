7b68d303843bb29ac95beb78623d2ca5282ad7f9
Vincent <37227569+Vinbug1@users.noreply.github.com>
2022-07-20 00:07:01 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,23 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-* _print_rev_recursion - prints string in reverse
-* @s: the string
-* Return: nothing
-*/
-void _print_rev_recursion(char *s)
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+
+void _puts_recursion(char *s)
 {
-if (*s != '\0')
+
+if (*s == '\0')
 {
-_print_rev_recursion(s + 1);
-_putchar(*s);
-}}
+	_putchar('\n');
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+}
